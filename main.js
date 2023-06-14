0d3b961875cb12dfb308f79c66e0a2c331ae591e
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-14 23:03:29 +0200

    Added some Html, CSS and Javascript(functionality)

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-
+function myFunction() {
+    let myPassWord = document.getElementById("input-password");
+    if (myPassWord.type === "password") {
+      myPassWord.type = "text";
+    } else {
+      myPassWord.type = "password";
+    }
+  }
 
 
 
