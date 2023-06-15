e233c6fdc12635ab28205494d1fa5931de9170c7
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-15 11:42:37 +0200

    Added html & Javascript

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+document.getElementById("button").onclick = function submitBtn(){
+  userName = document.getElementById("input-name");
+  userEmail = document.getElementById("input-email");
+  userAge = document.getElementById("input-age");
+
+  if(userName.value === ""){
+    alert(`Name field is required!`);
+  }if(userEmail.value === ""){
+    alert(`Email field is required!`);
+  }if(userAge.value === ""){
+    alert(`Age field is required!`);
+  }
+ 
+
+}
+
+
+
+
 function myFunction() {
     let myPassWord = document.getElementById("input-password");
     if (myPassWord.type === "password") {
@@ -6,6 +25,8 @@ function myFunction() {
       myPassWord.type = "password";
     }
   }
+
+   
 
 
 
